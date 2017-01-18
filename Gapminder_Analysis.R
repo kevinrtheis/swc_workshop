7b2b11ab60a4ff85d54579f680c9b6e46d3b3fdd
kevinrtheis <ktheis@med.wayne.edu>
2017-01-18 12:05:22 -0500

    made an edit for pulling

--- Gapminder_Analysis.R
+++ Gapminder_Analysis.R
@@ -2,6 +2,8 @@
 #Date: January 17-18, 2017
 #Word, 5000G
 
+#This file is for software carpentry
+
 download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv", destfile = "gapminder-FiveYearData.csv")
 gapminder <- read.csv("gapminder-FiveYearData.csv")
 
